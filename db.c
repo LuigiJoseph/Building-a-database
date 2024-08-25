4859e9abfda6f1829dcfd7a37537b4549946ba29
Mohammed <luigiornojoseph@gmail.com>
2024-08-25 22:01:04 +0300

    more data rows establishing

--- db.c
+++ db.c
@@ -7,15 +7,27 @@
 #define COLUMN_USERNAME_SIZE 32
 #define COLUMN_EMAIL_SIZE 255
 
+const uint32_t PAGE_SIZE = 4096;
+#define TABLE_MAX_PAGES = 100
+
+
 #define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
 
-const uint32_t ID_SIZE = size_of_attribute(Row, id);
-const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
-const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
-const uint32_t ID_OFFSET = 0;
-const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
-const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
-const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+#define ID_SIZE 4 // Example size for 'id'
+#define USERNAME_SIZE 32 // Example size for 'username'
+#define EMAIL_SIZE 64 // Example size for 'email'
+
+#define ID_OFFSET 0
+#define USERNAME_OFFSET (ID_OFFSET + ID_SIZE)
+#define EMAIL_OFFSET (USERNAME_OFFSET + USERNAME_SIZE)
+#define ROW_SIZE (ID_SIZE + USERNAME_SIZE + EMAIL_SIZE)
+
+
+const uint32_t PAGE_SIZE = 4096;
+#define TABLE_MAX_PAGES = 100
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE/ROW_SIZE;
+const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+
 typedef struct {
   uint32_t id;
   char username[COLUMN_USERNAME_SIZE];
@@ -122,6 +134,19 @@ void execute_statement(Statement* statement){
       printf("This is where we would do a select.\n");
       break;
   }
+}
+
+void serialize_row(Row* source, void* destination) {
+  memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
+  memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
+  memcpy(destination + EMAIL_OFFSET, &(source->username), EMAIL_SIZE);
+  
+}
+
+void deserialize_row(Row* source, Row* destination) {
+  memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
+  memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
+  memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_OFFSET);
 }
 
 // infinte loop for REPL
